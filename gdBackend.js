bf48e8d8cf80dfc00de0ff69bfc472e77310625c
marcievanauken <marcievanauken@gmail.com>
2022-12-02 13:13:43 -0500

    restructure project for demo

--- gdBackend.js
+++ gdBackend.js
@@ -1,23 +1,7 @@
 function gdProcess(){
 	getGifts(endpoint, timeParams);
-	truncateGifts(gifts, dbLengths);
+	validateGifts(gifts);
 	insertGifts(gifts);
 }
 
-function truncateGifts(gifts, dbLengths){
-	console.log(dbLengths);
-	for (gift in gifts){
-		/* 
-			//general validation checks
-			//validations and regex etc
-
-		*/
-
-		if (gift[i].lengthTooLong){
-			truncateVals(gift);
-		}
-
-		//adding some code
-	}
-
-}
+gdProcess();
