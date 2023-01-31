95b64bc922624d18b33e44d9748c9c081a03f10f
marcievanauken <marcievanauken@gmail.com>
2023-01-31 13:09:35 -0500

    custom action test

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const github = require('@actions/github');
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
-  console.log('MARCIE TYPING STUFF DID THIS SHOW IN CONSOLE');
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+  console.log('HEEEREEEEEEEE');
+  console.log(payload.issue.title)
+  console.log('HEEEREEEEEEEE');
 } catch (error) {
   core.setFailed(error.message);
 }
