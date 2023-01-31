7b84ebeb1e475d1b6d9fe85340d814dd2f2dbae7
marcievanauken <marcievanauken@gmail.com>
2023-01-31 13:17:43 -0500

    custom action test

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ try {
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   // console.log(`The event payload: ${payload}`);
   console.log('HEEEREEEEEEEE');
-  console.log(payload)
+  console.log(payload.issue)
   console.log('HEEEREEEEEEEE');
 } catch (error) {
   core.setFailed(error.message);
