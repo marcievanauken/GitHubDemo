0b30de5a011a4f15ac843df0a92910027eff8ee4
marcievanauken <marcievanauken@gmail.com>
2023-02-01 15:34:10 -0500

    webhook

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// const core = require('@actions/core');
-// const github = require('@actions/github');
 const { Octokit } = require("@octokit/rest");
 const { Webhooks, createNodeMiddleware } = require("@octokit/webhooks");
 const EventSource = require('eventsource'); // local dev: watches for an event from the URL source (webhook url)
@@ -7,12 +5,12 @@ const config = require('./config.js');
 const owner = config.ghCreds.owner;
 const repo = config.ghCreds.repo;
 
-const webhooks = new Webhooks({
-  secret: "mva-secret-052387",
-});
-
 const octokit = new Octokit({
     auth: config.ghCreds.accessToken
+});
+
+const webhooks = new Webhooks({
+  secret: config.ghCreds.secret,
 });
 
 const webhookProxyUrl = "https://smee.io/oQVZEiZC5N9juPAH"; //local dev to read event payload
@@ -49,30 +47,6 @@ async function createBranch(payload) {
 		// console.log(createBranch)
 	} 
 	catch (error) {
-	  core.setFailed(error.message);
-	}
-}
-
-// createBranch();
-
-
-// creating custom action old code
-var usingGitHub = false;
-if (usingGitHub){
-	try {
-	  // `who-to-greet` input defined in action metadata file
-	  const nameToGreet = core.getInput('who-to-greet');
-	  console.log(`Hello ${nameToGreet}!`);
-	  const time = (new Date()).toTimeString();
-	  core.setOutput("time", time);
-	  // Get the JSON webhook payload for the event that triggered the workflow
-	  const payload = JSON.stringify(github.context.payload, undefined, 2)
-	  // console.log(`The event payload: ${payload}`);
-	  console.log('HEEEREEEEEEEE');
-	  console.log(github.context.payload.issue.title) // issue event
-	  console.log('HEEEREEEEEEEE');
-	} 
-	catch (error) {
-	  core.setFailed(error.message);
+	  console.log(error);
 	}
 }
