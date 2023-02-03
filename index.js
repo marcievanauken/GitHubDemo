a29b0e0748fe48c8ea0dddd73b00c0d9330d7b68
marcievanauken <marcievanauken@gmail.com>
2023-02-03 14:51:40 -0500

    test

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ async function tagBranch(e) {
 		const createTagObj = await octokit.request('POST /repos/{owner}/{repo}/git/tags', {
 		  owner: owner,
 		  repo: repo,
-		  tag: '1.0.0', // to be variable - calculated based on labels?
+		  tag: '1.0.1', // to be variable - calculated based on labels?
 		  message: 'tag main branch',
 		  object: fetchRef.data.object.sha,
 		  type: 'commit'
@@ -78,7 +78,7 @@ async function tagBranch(e) {
 		const createTag = await octokit.request('POST /repos/{owner}/{repo}/git/refs', {
 		  owner: owner,
 		  repo: repo,
-		  ref: 'refs/tags/1.0.0', // to be variable
+		  ref: 'refs/tags/1.0.1', // to be variable
 		  sha: createTagObj.data.object.sha
 		});
 		console.log(`Main Branch Tagged: ${createTag.data.ref}`);
